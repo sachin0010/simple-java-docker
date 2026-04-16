5f81258c82dacbe86c87923bb1856bb00560e850
Sachin Tomar <35167656+sachin0010@users.noreply.github.com>
2026-04-16 13:21:48 +0530

    Change greeting message in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+        System.out.println("Hello, Sachin! Current date: " + currentDate);
     }
 }
